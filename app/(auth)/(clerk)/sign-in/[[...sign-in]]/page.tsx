e01bbc80869ee4d6d14c00297d6be7eba6d44b3d
CodesNiAyban <101535798+CodesNiAyban@users.noreply.github.com>
2024-05-21 04:59:28 +0800

    pakagaling

--- app/(auth)/(clerk)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(clerk)/sign-in/[[...sign-in]]/page.tsx
@@ -96,9 +96,9 @@ export default function SignInPage() {
                       </Button>
                     </SignIn.Action>
 
-                    <Button variant="link" size="sm" asChild>
+                    {/* <Button variant="link" size="sm" asChild>
                       <Link href="/sign-up">Don&apos;t have an account? Sign up</Link>
-                    </Button>
+                    </Button> */}
                   </div>
                 </CardFooter>
               </Card>
