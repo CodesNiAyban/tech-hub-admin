30394f55ef36afc3a150c9c35881ed6cdad03c1e
CodesNiAyban <101535798+CodesNiAyban@users.noreply.github.com>
2024-06-11 17:49:33 +0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import { siteConfig } from "@/config/site";
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
 import "@uploadthing/react/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { checkRole } from "@/lib/role";
+import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,7 +37,6 @@ export default function RootLayout({
         },
         baseTheme: dark
       }}
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <html lang="en">
         <body className={inter.className}>
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in', '/api(.*)']);
+
 export default clerkMiddleware((auth, req) => {
   // Add custom logic to run before redirecting // Add notification that session expired
   if (!isPublicRoute(req) && !auth().userId) {
