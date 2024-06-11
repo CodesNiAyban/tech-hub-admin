662179e79ca75ee73e2ec5ae39b1ef3b128398eb
CodesNiAyban <101535798+CodesNiAyban@users.noreply.github.com>
2024-06-12 01:44:17 +0800

    Revert "PLease work"
    This reverts commit 6d7fb82969408bc7cbfe05f842174a10621b4538.

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "utfs.io",
-                pathname: "**",
-              },
-            {
-                protocol: 'https',
-                hostname: 'img.clerk.com',
-                pathname: '**',
-            },
-        ],
-    },
+        domains: [
+            "utfs.io",
+            'img.clerk.com',
+        ]
+    }
 };
 
 export default nextConfig;
