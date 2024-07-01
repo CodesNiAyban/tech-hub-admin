f5df78f9084b14d012bcb5fde9879e3f3788c855
CodesNiAyban <101535798+CodesNiAyban@users.noreply.github.com>
2024-07-01 22:51:33 +0800

    auth errror

--- app/api/courses/[courseId]/chapters/[chapterId]/quiz/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/quiz/route.ts
@@ -9,20 +9,9 @@ export async function PATCH(
     { params }: { params: { courseId: string; chapterId: string } }
 ) {
     try {
-        const { userId } = auth();
-
-        if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 });
-        }
-
-        const courseOwner = await db.course.findUnique({
-            where: {
-                id: params.courseId,
-                userId: userId,
-            },
-        });
-
-        if (!courseOwner) {
+        const { sessionClaims } = auth();
+        
+        if (sessionClaims && sessionClaims.role !== "admin") {
             return new NextResponse("Unauthorized", { status: 401 });
         }
 
